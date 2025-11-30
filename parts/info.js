a6f40f667721eb1aab8b6d8a4c19c46be40ba4aa
zedongjia <zedongjia305@gmail.com>
2025-11-30 15:57:30 +0800

    brief

---
+++ parts/info.js
@@ -0,0 +1,57 @@
+import { ref, onMounted } from 'vue';
+const template = `
+<div class="part info">
+    <h2>Hi~👋</h2>
+    <div>I am currently 1st graduate student at the Institute for Network Sciences and Cyberspace at Tsinghua University.</div>
+    <br/>
+    <div v-for="info in infos">
+        <div class="title">{{info.intro}}</div>
+        <ul class="enum">
+            <li v-for="item in info.items" v-html="form_text(item)"></li>
+        </ul>
+    </div>
+</div>
+`;
+
+function setup() {
+    const infos = ref([]);
+    onMounted(() => {
+        fetch('../data/infos.json')
+            .then((response) => {
+                response
+                    .json()
+                    .then((data) => {
+                        infos.value = data;
+                    })
+                    .catch((e) => {
+                        console.log('Fail to parse data: ', e);
+                        infos.value = [];
+                    });
+            })
+            .catch((e) => {
+                console.log('Fail to load data: ', e);
+                infos.value = [];
+            });
+    });
+    function form_text(contents) {
+        let text = '';
+        for (let index = 0; index < contents.length; index++) {
+            let content = contents[index];
+            if (typeof content == 'object') {
+                text += `<a href="${content.href}" target="_blank">${content.text}</a>`;
+            } else {
+                text += content;
+            }
+        }
+        return text;
+    }
+    return {
+        infos,
+        form_text
+    };
+}
+
+export default {
+    template: template,
+    setup
+};
---
+++ parts/pub.js
@@ -0,0 +1,52 @@
+import { ref } from 'vue';
+const template = `
+<div class="part pub">
+    <h2>Publications</h2>
+    <ul v-if="papers.length!=0">
+        <li v-for="(paper, index) in papers" class="paper">
+            <div class="title">{{index+1}}. {{paper.title}}</div>
+            <div v-html="paper.author" class="author"></div>
+            <div v-html="paper.where"></div>
+            <div class="row">
+                <span class="label">{{paper.year}}</span>
+                <span :class="{'label': true, 'ccf-a':paper.ccf=='A', 'ccf-b':paper.ccf=='B'}">CCF-{{paper.ccf}}</span>
+                <span v-for="field in paper.fields" class="label">{{field}}</span>
+            </div>
+            <div class="action">
+                <a target="_blank" :href="paper.paper" class="btn">paper</a>
+                <a target="_blank" :href="paper.code" class="btn">code</a>
+            </div>
+        </li>
+    </ul>
+    <div class="empty" v-else>EMPTY</div>
+</div>
+`;
+
+function setup() {
+    const papers = ref([]);
+    [].length;
+    fetch('../data/pubs.json')
+        .then((response) => {
+            response
+                .json()
+                .then((data) => {
+                    papers.value = data.sort((a, b) => b.year - a.year);
+                })
+                .catch((e) => {
+                    console.log('Fail to parse data: ', e);
+                    papers.value = [];
+                });
+        })
+        .catch((e) => {
+            console.log('Fail to load data: ', e);
+            papers.value = [];
+        });
+    return {
+        papers
+    };
+}
+
+export default {
+    template: template,
+    setup
+};
